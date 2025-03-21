6eb6121cd8bd17b71a59fbcea89fccf17731a010
Leo Wehrfritz <leocommander.cs@gmail.com>
2025-03-21 10:10:49 +0100

    Added method to export logs

--- Sources/EasyErrorHandling/ErrorHandler.swift
+++ Sources/EasyErrorHandling/ErrorHandler.swift
@@ -56,6 +56,18 @@ public class ErrorHandler: ObservableObject {
         self.toasts = []
     }
     
+    public func exportLogs() throws -> [String] {
+        let store = try OSLogStore(scope: .currentProcessIdentifier)
+        let date = Date.now.addingTimeInterval(-24 * 3600)
+        let position = store.position(date: date)
+        
+        return try store
+            .getEntries(at: position)
+            .compactMap { $0 as? OSLogEntryLog }
+            .filter { $0.subsystem == Bundle.main.bundleIdentifier! }
+            .map { "[\($0.date.formatted())] [\($0.category)] \($0.composedMessage)" }
+    }
+    
     /**
      Handle an error.
      - Parameters:
--- Sources/EasyErrorHandling/Logs/ExportLogsButton.swift
+++ Sources/EasyErrorHandling/Logs/ExportLogsButton.swift
@@ -22,15 +22,7 @@ public struct ExportLogsButton: View {
     
     private func exportLogs() {
         do {
-            let store = try OSLogStore(scope: .currentProcessIdentifier)
-            let date = Date.now.addingTimeInterval(-24 * 3600)
-            let position = store.position(date: date)
-            
-            entries = try store
-                .getEntries(at: position)
-                .compactMap { $0 as? OSLogEntryLog }
-                .filter { $0.subsystem == Bundle.main.bundleIdentifier! }
-                .map { "[\($0.date.formatted())] [\($0.category)] \($0.composedMessage)" }
+            entries = try self.errorHandler.exportLogs()
         } catch {
             errorHandler.handle(error, while: "exporting logs")
         }
