60451cfc4d6b167a800a24b798f771785c9b5d58
Leo Wehrfritz <leo@wehrfritz.me>
2025-08-29 13:46:26 +0200

    Changed log display to use monospaced font

--- Sources/EasyErrorHandling/Logs/LogDisplay.swift
+++ Sources/EasyErrorHandling/Logs/LogDisplay.swift
@@ -37,6 +37,7 @@ public struct LogDisplay: View {
                 LazyVStack(alignment: .leading) {
                     ForEach(entries.reversed()) { entry in
                         Text(entry.entry)
+                            .font(.system(size: 12, design: .monospaced))
                     }
                 }
             }
@@ -67,4 +68,19 @@ public struct LogDisplay: View {
             }
         }
     }
+}
+
+
+#Preview {
+    LogDisplay(
+        entries: [
+            "Entry A",
+            "Entry B",
+            "Entry C",
+            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+        ],
+        title: "Logs",
+        dismissButtonText: "Dismiss",
+        copyToClipboardButtonText: "Copy to clipboard"
+    )
 }
