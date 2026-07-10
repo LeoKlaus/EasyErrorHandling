3ead9bbddd6515cb6381d6a41914ab033c019f05
Leo Wehrfritz <leo@wehrfritz.me>
2026-07-10 12:21:55 +0200

    (Breaking!) Updated ImpactGenerator

--- Sources/EasyErrorHandling/ImpactGenerator.swift
+++ Sources/EasyErrorHandling/ImpactGenerator.swift
@@ -5,9 +5,60 @@
 //  Created by Leo Wehrfritz on 13.03.25.
 //
 
-
 import Foundation
 import SwiftUI
+
+public enum DeviceAgnosticImpactStyle {
+    case light, medium, heavy, soft, rigid
+    
+#if os(iOS)
+    var deviceSpecificValue: UIImpactFeedbackGenerator.FeedbackStyle {
+        switch self {
+        case .light:
+                .light
+        case .medium:
+                .medium
+        case .heavy:
+                .heavy
+        case .soft:
+                .soft
+        case .rigid:
+                .soft
+        }
+    }
+#elseif os(watchOS)
+    var deviceSpecificValue: WKHapticType {
+        .click
+    }
+#endif
+}
+public enum DeviceAgnosticImpactType {
+    case error, success, warning
+    
+#if os(iOS)
+    var deviceSpecificValue: UINotificationFeedbackGenerator.FeedbackType {
+        switch self {
+        case .error:
+                .error
+        case .success:
+                .success
+        case .warning:
+                .warning
+        }
+    }
+#elseif os(watchOS)
+    var deviceSpecificValue: WKHapticType {
+        switch self {
+        case .error:
+                .failure
+        case .success:
+                .success
+        case .warning:
+                .retry
+        }
+    }
+#endif
+}
 
 #if os(iOS) && !targetEnvironment(simulator)
 /// A helper to simplify working with haptic feedback
@@ -15,12 +66,25 @@ import SwiftUI
 public final class ImpactGenerator {
     public static let shared = ImpactGenerator()
     
-    public func impactOccured(style: UIImpactFeedbackGenerator.FeedbackStyle = .medium) {
-        UIImpactFeedbackGenerator(style: style).impactOccurred()
+    public func impactOccured(style: DeviceAgnosticImpactStyle = .medium) {
+        UIImpactFeedbackGenerator(style: style.deviceSpecificValue).impactOccurred()
     }
     
-    public func notify(type: UINotificationFeedbackGenerator.FeedbackType = .success) {
-        UINotificationFeedbackGenerator().notificationOccurred(type)
+    public func notify(type: DeviceAgnosticImpactType = .success) {
+        UINotificationFeedbackGenerator().notificationOccurred(type.deviceSpecificValue)
+    }
+}
+#elseif os(watchOS) && !targetEnvironment(simulator)
+@MainActor
+public final class ImpactGenerator {
+    public static let shared = ImpactGenerator()
+    
+    public func impactOccured(style: DeviceAgnosticImpactStyle = .medium) {
+        WKInterfaceDevice.current().play(style.deviceSpecificValue)
+    }
+    
+    public func notify(type: DeviceAgnosticImpactType = .success) {
+        WKInterfaceDevice.current().play(type.deviceSpecificValue)
     }
 }
 #endif
